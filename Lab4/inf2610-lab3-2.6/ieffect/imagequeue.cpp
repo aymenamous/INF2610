d7d1f0b8066dbe45e08a15c55009d6dfb44a51c5
inavarroa <ingrid.navarro.an@gmail.com>
2017-11-20 16:51:47 -0500

    started ex 1

---
+++ Lab4/inf2610-lab3-2.6/ieffect/imagequeue.cpp
@@ -0,0 +1,74 @@
+#include <QImage>
+#include <QDebug>
+#include <QQueue>
+
+#include "imagequeue.h"
+#include "simpletracer.h"
+
+ImageQueue::ImageQueue(QObject *parent, int capacity) :
+    QObject(parent),
+    m_capacity(capacity)
+{
+    // https://stackoverflow.com/questions/11706985/win32-thread-safe-queue-implementation-using-native-windows-api
+    // http://www.cs.wustl.edu/~schmidt/win32-cv-1.html
+
+    // CreateSemaphore :
+    // ARGS : security, init val, fina val, anonym
+
+    // semaphore enqueue
+    hSem1 = CreateSemaphore(NULL, capacity, capacity, NULL);
+    // semaphore dequeue
+    hSem2 = CreateSemaphore(NULL, capacity, capacity, NULL);
+
+    // CreateMutex :
+    // ARGS : security, owned, unnamed
+    hLock = CreateMutex(NULL, FALSE, NULL);
+}
+
+ImageQueue::~ImageQueue()
+{
+    CloseHandle(hSem1);
+    CloseHandle(hSem2);
+    CloseHandle(hLock);
+   // queue.removeAll();
+}
+
+void ImageQueue::enqueue(QImage *item)
+{
+    DWORD waitResult;
+    // Attendre
+    WaitForSingleObject(hSem1, INFINITE);
+    waitResult = WaitForSingleObject(hLock, INFINITE);
+
+    if (waitResult == WAIT_OBJECT_0) {
+         queue.enqueue(item);
+         // tracer la taille de la file lorsqu'elle change
+         SimpleTracer::writeEvent(this, 0);
+         // Liberer mutex (release ownership)
+         ReleaseMutex(hLock);
+         // Incrementer valeur du semaphore
+         ReleaseSemaphore(hSem1, 1, NULL);
+    }
+}
+
+QImage *ImageQueue::dequeue()
+{
+    DWORD waitResult;
+    // Attendre
+    WaitForSingleObject(hSem2, INFINITE);
+    waitResult = WaitForSingleObject(hLock, INFINITE);
+
+    if (waitResult == WAIT_OBJECT_0) {
+         QImage* item = queue.dequeue();
+         // tracer la taille de la file lorsqu'elle change
+         SimpleTracer::writeEvent(this, 0);
+         // Liberer mutex (release ownership)
+         ReleaseMutex(hLock);
+         // Incrementer valeur du semaphore
+         ReleaseSemaphore(hSem2, 1, NULL);
+    }
+
+    // tracer la taille de la file lorsqu'elle change
+    // SimpleTracer::writeEvent(this, 0);
+    return NULL;
+}
